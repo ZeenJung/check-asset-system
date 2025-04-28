db855e6d708313025d42e4b87fae5d7d9be74c52
ZeenJung <6531501145@lamduan.mfu.ac.th>
2025-04-28 13:21:11 +0700

    first commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,31 @@
+<template>
+  <header class="navbar">
+    <div class="navbar-title">ระบบตรวจครุภัณฑ์ ประจำปี 2568</div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
+
+<style scoped>
+.navbar {
+  width: 100%; /* ยาวเต็มจอ */
+  background-color: #03126A;
+  padding: 16px 24px; /* padding บนล่าง 16px, ซ้ายขวา 24px */
+  box-sizing: border-box; /* กัน padding ดันความกว้างเกิน 100% */
+  position: fixed; /* ติดอยู่ด้านบน */
+  top: 0;
+  left: 0;
+  z-index: 1000; /* อยู่บนสุด */
+}
+
+.navbar-title {
+  color: white;
+  font-size: 24px;
+  font-weight: bold;
+  text-align: left; /* ชิดซ้าย */
+}
+</style>
