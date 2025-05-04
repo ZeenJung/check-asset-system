f9b04a6e8d2271c6e188a6b559bfc78a050b34bb
ZeenJung <6531501145@lamduan.mfu.ac.th>
2025-05-04 15:35:33 +0700

    Create Login.vue

---
+++ src/components/Login.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container mt-4">
+      <h3 class="mb-3">รายการทรัพย์สิน</h3>
+      <table class="table table-bordered table-striped">
+        <thead class="table-dark">
+          <tr>
+            <th>Picture</th>
+            <th>Inventory number</th>
+            <th>Asset description</th>
+            <th>Location</th>
+            <th>Room</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+                <img src="/src/assets/Table0001.jpg" alt="Table" width="100" />
+            </td>
+            <td>711000702743014</td>
+            <td>โต๊ะทำงาน ขนาด 160 x 80 x 75 ซม.</td>
+            <td>E1</td>
+            <td>306</td>
+            <td><span class="badge bg-success">Normal</span></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </template>
+  
