92898ee03cd9ad311db14a94d55fc209eab30331
ZeenJung <6531501145@lamduan.mfu.ac.th>
2025-05-04 17:49:11 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,5 +3,43 @@ import './style.css'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-
 createApp(App).mount('#app')
+
+export default {
+  data() {
+    return {
+      showQRModal: false,
+      qrResult: "",
+      qrScanner: null,
+    };
+  },
+  methods: {
+    openScanner() {
+      this.showQRModal = true;
+
+      this.$nextTick(() => {
+        this.qrScanner = new Html5Qrcode("qr-reader");
+        this.qrScanner.start(
+          { facingMode: "environment" },
+          { fps: 10, qrbox: 250 },
+          (decodedText) => {
+            this.qrResult = decodedText;
+            this.qrScanner.stop(); // หยุดเมื่อสแกนได้
+            // คุณสามารถเปิด Modal รายการทรัพย์สินจาก decodedText ตรงนี้
+          },
+          (errorMessage) => {
+            console.warn(errorMessage);
+          }
+        );
+      });
+    },
+    closeScanner() {
+      this.showQRModal = false;
+      if (this.qrScanner) {
+        this.qrScanner.stop().then(() => {
+          this.qrScanner.clear();
+        });
+      }
+    },
+  },
+};
