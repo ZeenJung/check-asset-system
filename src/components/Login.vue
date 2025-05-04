375d781e6907ca74e934a54eac28968e446ca2ad
ZeenJung <6531501145@lamduan.mfu.ac.th>
2025-05-04 16:08:43 +0700

    Update Login.vue

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,9 +1,18 @@
 <template>
     <header class="navbar">
-      <div class="navbar-title">ระบบตรวจครุภัณฑ์ ประจำปี 2568</div>
-    </header>
+  <div class="d-flex flex-column align-items-start">
+    <img src="/src/assets/ADT_MFU_Logo.png" alt="Table" width="100" />
+    <div class="text-primary small">School of Applied Digital Technology</div>
+  </div>
+
+  <div class="text-white ms-3">ระบบตรวจครุภัณฑ์ ประจำปี 2568</div>
+
+  <a href="#" class="btn text-white ms-auto" role="button">Login</a>
+
+</header>
+
     <div class="container mt-4">
-      <h3 class="mb-3">รายการทรัพย์สิน</h3>
+      <h3 class="badge text-bg-dark">รายการทรัพย์สิน</h3>
       <table class="table table-bordered table-striped">
         <thead class="table-dark">
           <tr>
@@ -38,7 +47,7 @@
             <td>โต๊ะทำงาน ขนาด 160 x 80 x 75 ซม.</td>
             <td>E1</td>
             <td>306</td>
-            <td><span class="badge bg-success">Normal</span></td>
+            <td><span class="badge bg-warning">Damaged</span></td>
           </tr>
         </tbody>
       </table>
@@ -47,22 +56,26 @@
   
   <style scoped>
   .navbar {
-    width: 100%; /* ยาวเต็มจอ */
-    background-color: #03126A;
-    padding: 16px 24px; /* padding บนล่าง 16px, ซ้ายขวา 24px */
-    box-sizing: border-box; /* กัน padding ดันความกว้างเกิน 100% */
-    position: fixed; /* ติดอยู่ด้านบน */
+    width: 100%;
+    background-color: #010939;
+    padding: 16px 24px;
+    box-sizing: border-box;
+    position: fixed;
     top: 0;
     left: 0;
-    z-index: 1000; /* อยู่บนสุด */
-  }
-  
-  .navbar-title {
-    color: white;
-    font-size: 24px;
-    font-weight: bold;
-    text-align: left; /* ชิดซ้าย */
-  }
+    z-index: 1000;
+}
+
+.navbar::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 20%;
+    height: 100%;
+    background-color: white;
+    z-index: -1; /* อยู่ใต้ navbar */
+}
   
   body {
     font-family: 'Sarabun', sans-serif;
